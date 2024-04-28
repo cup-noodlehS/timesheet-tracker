592d1f3992babe8a63cc7ec1219ac0c9c169a725
Sheldon Sagrado <sheldon@bposeats.com>
2024-04-28 19:57:14 +0800

    change the facicon

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,12 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <title>{metadata.title}</title>
+        <meta name="description" content={metadata.description} />
+        <link rel="stylesheet" href={inter.url} />
+        <link rel="icon" type="image/png" href="/icon.png" />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 'use client';
 
-import Image from "next/image";
 import { useState, useEffect, useRef } from "react";
 
 import useTimesheetStore from "@/store/timesheetStore";
